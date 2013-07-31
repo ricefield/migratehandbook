264ff855f10482383dd702f8955be760c1e9fe9e
Jonathan Tien <jonathan.tien@gmail.com>
2013-07-31 16:12:18 -0700

    some bug fixes and changes in structure. added duplicate else clause

--- omsmigrate.py
+++ omsmigrate.py
@@ -41,9 +41,7 @@ new_session = scoped_session(sessionmaker(autoflush=True, bind=new_engine))
 new_metadata = ThreadLocalMetaData()
 new_metadata.bind = new_engine
 
-""" MODEL DATA:
-	before we can begin doing anything, we need to first model all the tables we'll be dealing with
-"""
+""" MODEL DATA """
 
 # old
 class Distributorship(Entity):
@@ -114,11 +112,12 @@ def check_duplicate(distro, hbcity):
 
 
 for distro in Distributorship.query.all():
+
+	# migrate distributorship -> city, team
+	city, state = distro.Name.split(',')
+	state.strip()
+
 	if # not duplicate:
-
-		# migrate distributorship -> city, team
-		city, state = distro.Name.split()
-
 		# create city
 		newcity = City(city_name=city, city_state=state)
 		newcity.city_description = ""
@@ -126,52 +125,46 @@ for distro in Distributorship.query.all():
 		new_session.commit()
 
 		# create team
-		newteam = TeamNew(team_name=city.name+", "+city.state, 
+		newteam = Team(team_name=city+", "+state, 
 						  team_assigned_city=newcity.city_id)
 		new_session.add(newteam)
 		new_session.commit()
 
-		# mappings
-		distroy2city[distro.Id] = newcity.city_id
-		distro2team[distro.Id] = newteam.team_id
 
-		# migrate zip code assignments
-		for row in DistributorshipZipCode.query.filter_by(DistributorshipId=distro.Id).all():
-			# find zipcodes and create zipcode relationships
-			newzip = CityZipCodes(city_id=newcity.city_id,
-								 	 city_group_id=None,
-									 zipcode_id=ZipCode.query.filter_by(zip_code=row.Zipcode).one().id,
-									 zipcode=row.Zipcode,
-									 type=1)
-			new_session.add(newzip)
-			new_session.commit()
+# migrate zip code assignments
+for row in DistributorshipZipCode.query.filter_by(DistributorshipId=distro.Id).all():
+	# find zipcodes and create zipcode relationships
+	newzip = CityZipCodes(city_id=newcity.city_id,
+						 	 city_group_id=None,
+							 zipcode_id=ZipCode.query.filter_by(zip_code=row.Zipcode).one().id,
+							 zipcode=row.Zipcode,
+							 type=1)
+	new_session.add(newzip)
+	new_session.commit()
 
-		# create new users and roles
-		for user in DistributorshipUserInRole.filter_by(DistributorshipId=distro.Id).all():
+# create new users and roles
+for user in DistributorshipUserInRole.query.filter_by(DistributorshipId=distro.Id).all():
 
-			# create new user
-			newuser = UsersNew(email=user.Username,
-							   username=user.Username,
-							   created_on=datetime.datetime.now(),
-							   display_name=""
-			newuser.password_hash = "" # null, necessitates a reset
-			newuser.salt = "" # see above
-			# necessary bc new db doesn't allow NULL for this value
-			newuser.last_ip = ""
-			newuser.active = 1 # user shouldn't have to activate
-			newuser.activate_hash = "" # shouldn't be necessary
-			
-			# user permissions
-			if user.Role == 'Overseer':
-				newuser.role_id = 7
-			else:
-				newuser.role_id = 4
-
-			# commit 
-			new_session.add(newuser)
-			new_session.commit()
-
+	# create new user
+	newuser = UsersNew(email=user.Username,
+					   username=user.Username,
+					   created_on=datetime.datetime.now(),
+					   display_name="")
+	newuser.password_hash = "" # null, necessitates a reset
+	newuser.salt = "" # see above
+	# necessary bc new db doesn't allow NULL for this value
+	newuser.last_ip = ""
+	newuser.active = 1 # user shouldn't have to activate
+	newuser.activate_hash = "" # shouldn't be necessary
+	
+	# user permissions
+	if user.Role == 'Overseer':
+		newuser.role_id = 7
 	else:
-		# what to do if there's a duplicate?
+		newuser.role_id = 4
+
+	# commit 
+	new_session.add(newuser)
+	new_session.commit()
 
 
