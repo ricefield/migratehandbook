7a90286a1aeec02c85272c49182c06b75bff2acf
Jonathan Tien <jonathan.tien@gmail.com>
2013-07-31 12:26:00 -0700

    initial migration logic

--- omsmigrate.py
+++ omsmigrate.py
@@ -98,3 +98,80 @@ logging.info("### successfully generated Python models ###")
 
 logging.info("### beginning migration of data ###")
 
+distro2city = {}
+distro2team = {}
+
+
+def check_duplicate(distro, hbcity):
+	""" takes a distributorship name from the oms and checks to see
+	if there's an identical city in the handbook
+	"""
+	try:
+		city, state = distro.split()
+		if 
+	except ValueError:
+		return True
+
+
+for distro in Distributorship.query.all():
+	if # not duplicate:
+
+		# migrate distributorship -> city, team
+		city, state = distro.Name.split()
+
+		# create city
+		newcity = City(city_name=city, city_state=state)
+		newcity.city_description = ""
+		new_session.add(newcity)
+		new_session.commit()
+
+		# create team
+		newteam = TeamNew(team_name=city.name+", "+city.state, 
+						  team_assigned_city=newcity.city_id)
+		new_session.add(newteam)
+		new_session.commit()
+
+		# mappings
+		distroy2city[distro.Id] = newcity.city_id
+		distro2team[distro.Id] = newteam.team_id
+
+		# migrate zip code assignments
+		for row in DistributorshipZipCode.query.filter_by(DistributorshipId=distro.Id).all():
+			# find zipcodes and create zipcode relationships
+			newzip = CityZipCodes(city_id=newcity.city_id,
+								 	 city_group_id=None,
+									 zipcode_id=ZipCode.query.filter_by(zip_code=row.Zipcode).one().id,
+									 zipcode=row.Zipcode,
+									 type=1)
+			new_session.add(newzip)
+			new_session.commit()
+
+		# create new users and roles
+		for user in DistributorshipUserInRole.filter_by(DistributorshipId=distro.Id).all():
+
+			# create new user
+			newuser = UsersNew(email=user.Username,
+							   username=user.Username,
+							   created_on=datetime.datetime.now(),
+							   display_name=""
+			newuser.password_hash = "" # null, necessitates a reset
+			newuser.salt = "" # see above
+			# necessary bc new db doesn't allow NULL for this value
+			newuser.last_ip = ""
+			newuser.active = 1 # user shouldn't have to activate
+			newuser.activate_hash = "" # shouldn't be necessary
+			
+			# user permissions
+			if user.Role == 'Overseer':
+				newuser.role_id = 7
+			else:
+				newuser.role_id = 4
+
+			# commit 
+			new_session.add(newuser)
+			new_session.commit()
+
+	else:
+		# what to do if there's a duplicate?
+
+
